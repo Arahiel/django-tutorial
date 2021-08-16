090321298bbe8ca20616f3805fe10d3e54bd53ba
Sebastian Tobolski <sebastian.tobolski@globallogic.com>
2021-08-16 08:31:39 +0200

    Changed static routes to dynamic with variable

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,8 +1,25 @@
-from django.http.response import HttpResponse
+from django.http import HttpResponse
+from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
-from django.http import HttpRequest
 # Create your views here.
 
+challengesDict = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes a day!",
+    "march": "Learn Django for at least 20 minutes a day!",
+    "april": "Eat no meat for the entire month",
+    "may": "Eat no meat for the entire month",
+    "june": "Eat no meat for the entire month",
+    "july": "Eat no meat for the entire month",
+    "august": "Eat no meat for the entire month",
+    "september": "Eat no meat for the entire month",
+    "october": "Eat no meat for the entire month",
+    "november": "Eat no meat for the entire month",
+    "december": "Eat no meat for the entire month",
+}
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge(request, month):
+    if month in challengesDict.keys():
+        return HttpResponse(challengesDict[month])
+    else:
+        return HttpResponseNotFound("This month is not supported!")
