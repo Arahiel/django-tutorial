9f779acda39dddc9af88ed8e19fe6d06c4d384ed
Sebastian Tobolski <sebastian.tobolski@globallogic.com>
2021-08-17 09:50:45 +0200

    Added django debug launch.json, path converters (urls) and another route for int:month + HttpRedirect

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.http.response import HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 # Create your views here.
 
@@ -8,17 +7,28 @@ challengesDict = {
     "february": "Walk for at least 20 minutes a day!",
     "march": "Learn Django for at least 20 minutes a day!",
     "april": "Eat no meat for the entire month",
-    "may": "Eat no meat for the entire month",
-    "june": "Eat no meat for the entire month",
+    "may": "Walk for at least 20 minutes a day!",
+    "june": "Learn Django for at least 20 minutes a day!",
     "july": "Eat no meat for the entire month",
-    "august": "Eat no meat for the entire month",
-    "september": "Eat no meat for the entire month",
+    "august": "Walk for at least 20 minutes a day!",
+    "september": "Learn Django for at least 20 minutes a day!",
     "october": "Eat no meat for the entire month",
-    "november": "Eat no meat for the entire month",
-    "december": "Eat no meat for the entire month",
+    "november": "Walk for at least 20 minutes a day!",
+    "december": "Learn Django for at least 20 minutes a day!",
 }
 
-def monthly_challenge(request, month):
+
+def monthly_challenge_by_number(request, month: int):
+    months = list(challengesDict.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month!")
+        
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect(f"/challenges/{redirect_month}")
+
+
+def monthly_challenge(request, month: str):
     if month in challengesDict.keys():
         return HttpResponse(challengesDict[month])
     else:
