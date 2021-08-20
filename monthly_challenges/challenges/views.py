e6f1c06fcb12d2361eb4e6bfa3effbeef08f07b0
Sebastian Tobolski <sebastian.tobolski@globallogic.com>
2021-08-20 09:19:14 +0200

    -Added name to index page -Reworked pages to render via dynamic templates -Added common templates directory in the root -Added base template (in common templates directory) -Added template header snippet

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.template.loader import render_to_string
 # Create your views here.
 
 challengesDict = {
@@ -16,24 +15,22 @@ challengesDict = {
     "september": "Learn Django for at least 20 minutes a day!",
     "october": "Eat no meat for the entire month",
     "november": "Walk for at least 20 minutes a day!",
-    "december": "Learn Django for at least 20 minutes a day!",
+    "december": None
 }
 
+
 def index(request):
-    ul = lambda input: f"<ul>\n{input}\n</ul>"
-    li = lambda input: f"<li>{input}</li>"
-    a = lambda name, link: f"<a href=\"{link}\">{name}</a>"
-    abs_url = lambda relative_url: request.build_absolute_uri(relative_url)
-    
-    list_html = ul("\n".join([li(a(el, abs_url(reverse("month-challenge", args=[el])))) for el in list(challengesDict.keys())]))
-    return HttpResponse(list_html)
+    return render(request, "challenges/index.html", {
+        "month_list": list(challengesDict.keys())
+    })
+
 
 def monthly_challenge_by_number(request, month: int):
     months = list(challengesDict.keys())
-    
+
     if month > len(months):
         return HttpResponseNotFound("Invalid month!")
-        
+
     redirect_month = months[month - 1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
@@ -42,7 +39,9 @@ def monthly_challenge_by_number(request, month: int):
 def monthly_challenge(request, month: str):
     if month in challengesDict.keys():
         challenge_text = challengesDict[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month": month
+        })
     else:
         return HttpResponseNotFound("This month is not supported!")
